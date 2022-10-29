68490cb7c6dd41a8f0b897d642d96556b72369c7
Martin Creedy <martin@openskydevelopment.co.uk>
2022-10-29 21:21:36 +0100

    Get id from handle

--- frontend/pages/space/[handle].tsx
+++ frontend/pages/space/[handle].tsx
@@ -1,14 +1,23 @@
-import DiscussionTopics from "../../components/app/discussion/DiscussionTopics";
-import DiscussionTopicComments from "../../components/app/discussion/DiscussionTopicComments";
 import AppLayout from "../../components/AppLayout";
 import { useRouter } from "next/router";
 import { useContext } from "react";
 import { SpaceContext } from "../../lib/SpaceContext";
 import Discussion from "../../components/app/discussion";
+import { chains } from "@web3modal/ethereum";
+import { useContractRead } from "@web3modal/react";
+import embraceSpacesContract from "../../data/contractArtifacts/EmbraceSpaces.json";
 
 export default function SpaceViewPage() {
   const [spaceId, setSpaceId] = useContext(SpaceContext);
   const handle = useRouter().query.handle;
+
+  const config = {
+    address: process.env.NEXT_PUBLIC_SPACES_CONTRACT_ADDRESS!,
+    abi: embraceSpacesContract.abi,
+    functionName: "getIdFromHandle",
+    chainId: chains.goerli.id,
+  };
+  const { data, error, isLoading, refetch } = useContractRead(config);
 
   // use handle to get spaceId from contract
   const spaceIdFromHandle = 99999;
