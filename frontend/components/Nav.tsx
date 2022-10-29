104f5b3fba0cf83e86329b20b9132a345777d65a
bennisan86 <info@bennisan.be>
2022-10-29 15:56:29 +0100

    test commit

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -29,7 +29,7 @@ export default function Nav() {
           <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
-                <div className="flex-shrink-0">Logo</div>
+                {/* <div className="flex-shrink-0">Logo</div> */}
                 <div className="hidden md:block">
                   <div className="ml-10 flex items-baseline space-x-4">
                     {navigation.map((item) => (
