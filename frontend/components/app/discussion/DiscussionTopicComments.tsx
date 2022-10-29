1caac4fceccfffee1d0d349cde71f78feb7cc63b
Martin Creedy <martin@openskydevelopment.co.uk>
2022-10-29 19:26:29 +0100

    Ceramic Comments Mutation

--- frontend/components/app/discussion/DiscussionTopicComments.tsx
+++ frontend/components/app/discussion/DiscussionTopicComments.tsx
@@ -1,5 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
+import { ThreeIdConnect } from "@3id/connect";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { useAccount } from "@web3modal/react";
 import { useContext, useState } from "react";
+import { useAuthenticateCeramic } from "../../../hooks/useAuthenticateCeramic";
+import { CeramicContext } from "../../../lib/CeramicContext";
 import { SpaceContext } from "../../../lib/SpaceContext";
 import DiscussionTopicComment from "./DiscussionTopicComment";
 
@@ -20,8 +24,34 @@ const DISCUSSION_TOPIC_COMMENT_QUERY = gql`
   }
 `;
 
+const DISCUSSION_TOPIC_COMMENT_MUTATION = gql`
+  mutation CreateNewDiscussionTopicComment(
+    $i: CreateDiscussionTopicCommentInput!
+  ) {
+    createDiscussionTopicComment(input: $i) {
+      document {
+        id
+        discussionTopicId
+        discussionTopic {
+          id
+          title
+        }
+        content
+        address
+        spaceId
+      }
+    }
+  }
+`;
+
 export default function DiscussionTopicComments() {
+  const threeId = new ThreeIdConnect();
+  const composeDbClient = useContext(CeramicContext);
+
   const [spaceId, setSpaceId] = useContext(SpaceContext);
+  const [title, setTitle] = useState("New Topic default title");
+  const [content, setContent] = useState("topic content");
+  const { account } = useAccount();
 
   const { data, loading, error } = useQuery(DISCUSSION_TOPIC_COMMENT_QUERY, {
     onCompleted: (data) => {
@@ -31,6 +61,37 @@ export default function DiscussionTopicComments() {
       console.log("error", error);
     },
   });
+
+  const [discussionTopicCommentMutation] = useMutation(
+    DISCUSSION_TOPIC_COMMENT_MUTATION,
+    {
+      onCompleted: (data) => {
+        alert("saved comment");
+      },
+      onError: (error) => {
+        console.log("error", error);
+      },
+    }
+  );
+
+  const createNewDiscussionTopicComment = async () => {
+    await useAuthenticateCeramic(threeId, composeDbClient);
+
+    discussionTopicCommentMutation({
+      variables: {
+        i: {
+          content: {
+            discussionTopicId:
+              "kjzl6kcym7w8ya26fgzodhklnsp5ivop1i3jevcz0nd5edzqhyhstnynjolw4wz",
+            content: "I like this",
+            address: "0x...Hdk",
+            spaceId: 99999,
+          },
+        },
+      },
+    });
+  };
+
   return (
     <>
       {loading && <div>Loading...</div>}
@@ -52,6 +113,13 @@ export default function DiscussionTopicComments() {
           </ul>
         </div>
       )}
+
+      <button
+        onClick={() => createNewDiscussionTopicComment()}
+        className="h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+      >
+        Create Comment
+      </button>
     </>
   );
 }
--- frontend/components/app/discussion/DiscussionTopics.tsx
+++ frontend/components/app/discussion/DiscussionTopics.tsx
@@ -1,7 +1,11 @@
-import { gql, useQuery } from "@apollo/client";
+import { ThreeIdConnect } from "@3id/connect";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { useAccount } from "@web3modal/react";
 import { useContext, useState } from "react";
+import { CeramicContext } from "../../../lib/CeramicContext";
 import { SpaceContext } from "../../../lib/SpaceContext";
 import DiscussionTopic from "./DiscussionTopic";
+import { useAuthenticateCeramic } from "../../../hooks/useAuthenticateCeramic";
 
 // We get all the topics and then filter on the frontend as
 // ComposeDB does not support filtering at this time
@@ -21,8 +25,28 @@ const DISCUSSION_TOPIC_QUERY = gql`
   }
 `;
 
+const DISCUSSION_TOPIC_MUTATION = gql`
+  mutation CreateNewDiscussionTopic($i: CreateDiscussionTopicInput!) {
+    createDiscussionTopic(input: $i) {
+      document {
+        id
+        title
+        content
+        address
+        spaceId
+      }
+    }
+  }
+`;
+
 export default function DiscussionTopics() {
+  const threeId = new ThreeIdConnect();
+  const composeDbClient = useContext(CeramicContext);
+
   const [spaceId, setSpaceId] = useContext(SpaceContext);
+  const [title, setTitle] = useState("New Topic default title");
+  const [content, setContent] = useState("topic content");
+  const { account } = useAccount();
 
   const { data, loading, error } = useQuery(DISCUSSION_TOPIC_QUERY, {
     onCompleted: (data) => {
@@ -32,6 +56,32 @@ export default function DiscussionTopics() {
       console.log("error", error);
     },
   });
+
+  const [discussionTopicMutation] = useMutation(DISCUSSION_TOPIC_MUTATION, {
+    onCompleted: (data) => {
+      alert("saved discussion topic");
+    },
+    onError: (error) => {
+      console.log("error", error);
+    },
+  });
+
+  const createNewDiscussionTopic = async () => {
+    await useAuthenticateCeramic(threeId, composeDbClient);
+
+    discussionTopicMutation({
+      variables: {
+        i: {
+          content: {
+            title,
+            content,
+            address: account.address,
+            spaceId,
+          },
+        },
+      },
+    });
+  };
 
   return (
     <>
@@ -55,6 +105,13 @@ export default function DiscussionTopics() {
           </ul>
         </div>
       )}
+
+      <button
+        onClick={() => createNewDiscussionTopic()}
+        className="h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+      >
+        Create Discussion
+      </button>
     </>
   );
 }
