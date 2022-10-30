cad343f934bbe34ff354283b4003b38784423f44
bennisan86 <info@bennisan.be>
2022-10-30 03:07:10 +0000

    space page

--- frontend/components/AppLayout.tsx
+++ frontend/components/AppLayout.tsx
@@ -3,13 +3,13 @@ import Nav from "./Nav";
 
 export default function AppLayout({ children, title }: any) {
   return (
-    <div className="bg-embracebg min-h-screen">
+    <div className="bg-white min-h-screen flex flex-col justify-start items-stretch content-stretch">
       <Head>
         <title>Embrace Community</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Nav />
-      <div className="min-h-full w-full">
+      <div className="min-h-full w-full bg-embracebg">
         <main>
           <div className="w-full">
             {children}
