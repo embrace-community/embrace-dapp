c4b167a7476def676fbfcdf19d6bd8d1bd0387de
bennisan86 <info@bennisan.be>
2022-11-16 17:53:33 +0100

    join spaces cleanup

--- frontend/components/space/Header.tsx
+++ frontend/components/space/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   MembershipGateToken,
   Access,
@@ -22,7 +23,7 @@ export default function Header({
   const access = Access[space.membership.access];
   const membershipGateToken = MembershipGateToken[space.membership.gate.token];
   const allowRequests = space.membership.allowRequests;
-
+  const [aboutShow, toggleAboutShow] = useState(false);
   console.log(space);
   console.log(
     access,
@@ -34,84 +35,109 @@ export default function Header({
 
   console.log(!membership?.isActive && space.membership.allowRequests);
   return (
-    <div className="w-full flex flex-col justify-start text-embracedark extrastyles-specialpadding2">
-      <div className="w-full flex flex-row justify-start items-end border-b-2 border-embracedark border-opacity-5 mb-12">
+    <div className="w-full flex flex-col extrastyles-specialpadding2">
+      <div className="w-full flex flex-col md:flex-row justify-start items-center md:items-start border-b-2 border-embracedark border-opacity-5 mb-4">
         {space?.metadata?.image ? (
           <img
-            className="w-28 h-28 extrastyles-border-radius extrastyles-negmarg"
+            className="w-20 h-20 extrastyles-border-radius extrastyles-negmarg-avatar bg-white"
             src={space?.metadata?.image}
           />
         ) : (
-          <span className="w-28 h-28 extrastyles-border-radius extrastyles-negmarg"></span>
+          <span className="w-28 h-28 extrastyles-border-radius extrastyles-negmarg-avatar"></span>
         )}
-        <div className="mb-6 ml-7">
-          <h1 className="font-semibold text-2xl">{space?.metadata?.name}</h1>
-          <div className="w-full flex flex-row mt-1 text-sm">
-            <p className="text-embracedark text-opacity-50 mx-3  mt-4px">
-              {space?.memberCount} Members
-            </p>
-          </div>
-          <div className="w-full flex flex-row mt-1 text-sm">
-            <p className="text-embracedark text-opacity-50 mx-3  mt-4px">
-              {space?.metadata?.description}
-            </p>
-          </div>
-          <div className="w-full flex flex-row mt-1 text-sm">
-            <p className="text-embracedark text-opacity-50 mx-3  mt-4px">
-              Visibility: {visibility}
-              <br />
-              Access: {access}
-              <br />
-              Membership Gate: {membershipGateToken}
-              <br />
-              Allow Requests: {allowRequests ? "Yes" : "No"}
-            </p>
+        <div className="w-full flex flex-col items-center md:items-start md:flex-row md:pl-7 md:pr-10 pb-5">
+          <div className="flex-1">
+            <h1 className="font-semibold text-2xl mb-1">{space?.metadata?.name}</h1>
+            <div className="">
+              <div className="w-full flex flex-row text-sm">
+              <p className="underline font-semibold" onClick={() => toggleAboutShow(!aboutShow)}>about</p>
+              <p className="text-embracedark opacity-20 mx-4">|</p>
+              {!membership?.isActive && <p className="text-embracedark opacity-50">You're not a member</p>}
+              {membership?.isActive &&
+                <div className="flex flex-row">
+                  <img
+                    className="h-5 w-5 rounded-full mr-3"
+                    src="https://api.multiavatar.com/Binx Bond.svg"
+                    alt=""
+                  />
+                  <p className="text-embracedark opacity-50">You're a member</p>
+                </div>
+              }
+              {membership?.isAdmin &&
+              <div className="flex flex-row">
+                <p className="text-embracedark opacity-50">You're an admin</p>
+              </div>
+              }
+              {isFounder && <div className="flex flex-row"><p className="text-embracedark opacity-50">You are the founder</p></div>}
+              </div>
+              <div className={aboutShow? "" : "hidden"}>
+                <div className="w-full flex flex-row mt-5 text-sm">
+                  <p className="text-embracedark">
+                    {space?.metadata?.description}
+                  </p>
+                </div>
+                <div className="w-full flex flex-row mt-2 text-sm ">
+                  <p className="text-embracedark">
+                    {space?.memberCount} Members
+                  </p>
+                </div>
+                <div className="w-full flex flex-row mt-1 text-sm">
+                  <p className="text-embracedark">
+                    Visibility: {visibility}
+                    <br />
+                    Access: {access}
+                    <br />
+                    Membership Gate: {membershipGateToken}
+                    <br />
+                    Allow Requests: {allowRequests ? "Yes" : "No"}
+                  </p>
+                </div>
+              </div>
+            </div>
 
+          </div>
+          
+          <div className="mt-6 mb-6 md:my-0">
             {/* When Public Open space and not a member allow joining */}
             {space.membership.access == Access.OPEN &&
               space.visibility == Visibility.PUBLIC &&
               !membership?.isActive && (
                 <button
-                  className="bg-embracelight text-embracedark text-sm font-semibold py-2 px-4 rounded-full ml-3"
+                  className="rounded-full border-violet-500 border-2 bg-transparent text-violet-500 text-sm font-semibold py-2 px-7"
                   onClick={() => joinSpace()}
                 >
-                  Join
+                  join space
                 </button>
               )}
 
             {/* When Private Closed space which allows requests. Address not a member and no pending request, then allow requests */}
-            {space.membership.access == Access.OPEN &&
-              space.visibility == Visibility.PUBLIC &&
+            {space.membership.access == Access.CLOSED &&
+              space.visibility == Visibility.PRIVATE &&
               space.membership.allowRequests &&
               !membership?.isActive &&
               !membership?.isRequest && (
                 <button
-                  className="bg-embracelight text-embracedark text-sm font-semibold py-2 px-4 rounded-full ml-3"
-                  onClick={() => requestJoinSpace()}
+                className="rounded-full border-violet-500 border-2 bg-transparent text-violet-500 text-sm font-semibold py-2 px-7"
+                onClick={() => requestJoinSpace()}
                 >
-                  Request to Join
+                  request to join
                 </button>
               )}
+              {membership?.isRequest && <p className="text-embracedark opacity-20 text-right">Request pending...</p>}
 
             {/* Gated space and not a member then allow join 
             TODO: Only show if the account meets the gate requirements*/}
             {space.membership.access == Access.GATED && !membership?.isActive && (
               <button
-                className="bg-embracelight text-embracedark text-sm font-semibold py-2 px-4 rounded-full ml-3"
-                onClick={() => requestJoinSpace()}
+              className="rounded-full border-violet-500 border-2 bg-transparent text-violet-500 text-sm font-semibold py-2 px-7"
+              onClick={() => requestJoinSpace()}
               >
-                Join Gated space
+                join gated space
               </button>
             )}
-
-            {membership?.isActive && <>You are a member</>}
-            {membership?.isRequest && <>Request pending</>}
-            {membership?.isAdmin && <>You are an Admin</>}
-
-            {isFounder && <>You are the founder</>}
           </div>
         </div>
       </div>
-    </div>
+      </div>
   );
 }
