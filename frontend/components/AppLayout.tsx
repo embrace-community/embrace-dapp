5340caf54007e425af10c11590cf948cee2b0e73
Tobias Leinss <caruso33@web.de>
2022-11-22 19:20:34 +0100

    Check current network of wallet

--- frontend/components/AppLayout.tsx
+++ frontend/components/AppLayout.tsx
@@ -1,16 +1,48 @@
 import Head from "next/head";
+import { ReactNode } from "react";
+import { Chain, useNetwork } from "wagmi";
+import { deployedChainId } from "../lib/envs";
 import Nav from "./Nav";
 
-export default function AppLayout({ children, title }: any) {
+export default function AppLayout({
+  children,
+  title,
+}: {
+  children: ReactNode;
+  title: ReactNode;
+}) {
+  const { chains, chain } = useNetwork();
+
+  const isOnWrongNetwork = chain?.id && chain.id !== Number(deployedChainId);
+
   return (
     <div className="bg-embracebg min-h-screen flex flex-col items-start justify-start">
       <Head>
         <title>Embrace Community: {title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+
       <Nav />
+
       <div className="w-full flex flex-1 flex-col justify-start items-start">
-        {children}
+        <main>
+          <div className="w-full">
+            {isOnWrongNetwork ? (
+              <div className="text-center text-embracedark">
+                <p className="text-2xl">
+                  Please switch to chain{" "}
+                  {chains?.find((c: Chain) => c.id === deployedChainId)?.name}
+                </p>
+
+                <p className="my-5 text-xl">
+                  You are currently on {chain?.name}
+                </p>
+              </div>
+            ) : (
+              children
+            )}
+          </div>
+        </main>
       </div>
     </div>
   );
--- frontend/lib/envs.ts
+++ frontend/lib/envs.ts
@@ -1,34 +1,45 @@
+const isNoTestEnvironment = process.env.NODE_ENV !== "test";
+
 // contracts
 
 const appContractAddress = process.env.NEXT_PUBLIC_APPS_CONTRACT_ADDRESS!;
 const spacesContractAddress = process.env.NEXT_PUBLIC_SPACES_CONTRACT_ADDRESS!;
 const accountsContractAddress =
   process.env.NEXT_PUBLIC_ACCOUNTS_CONTRACT_ADDRESS!;
+const deployedChainIdEnv = process.env.NEXT_PUBLIC_DEPLOYED_CHAIN_ID!;
 
-if (!appContractAddress && process.env.NODE_ENV !== "test")
+if (!appContractAddress && isNoTestEnvironment)
   throw Error("App Contract Address Env missing");
-if (!spacesContractAddress && process.env.NODE_ENV !== "test")
+if (!spacesContractAddress && isNoTestEnvironment)
   throw Error("Spaces Contract Address Env missing");
-if (!accountsContractAddress && process.env.NODE_ENV !== "test")
+if (!accountsContractAddress && isNoTestEnvironment)
   throw Error("Accounts Contract Address Env missing");
+
+if (
+  (!deployedChainIdEnv || !Number.isInteger(+deployedChainIdEnv)) &&
+  isNoTestEnvironment
+)
+  throw Error("Deployed Chain Id Env missing");
+const deployedChainId = Number(deployedChainIdEnv);
 
 // ipfs + blockchain connection
 
 const web3StorageKey = process.env.NEXT_PUBLIC_WEB3_STORAGE_KEY!;
 const infuraApiKey = process.env.NEXT_PUBLIC_INFURA_API_KEY!;
+const blockchainExplorerUrl = process.env.NEXT_PUBLIC_BLOCKEXPLORER_URL!;
 
-if (!web3StorageKey && process.env.NODE_ENV !== "test")
+if (!web3StorageKey && isNoTestEnvironment)
   throw Error("Web3 Storage Key Env missing");
-if (!infuraApiKey && process.env.NODE_ENV !== "test")
+if (!infuraApiKey && isNoTestEnvironment)
   throw Error("Infura Api Key Env missing");
-const blockchainExplorerUrl = process.env.NEXT_PUBLIC_BLOCKEXPLORER_URL;
-if (!blockchainExplorerUrl && process.env.NODE_ENV !== "test")
+if (!blockchainExplorerUrl && isNoTestEnvironment)
   throw Error("Blockchain Explorer Key Env missing");
 
 export {
   appContractAddress,
   spacesContractAddress,
   accountsContractAddress,
+  deployedChainId,
   web3StorageKey,
   infuraApiKey,
   blockchainExplorerUrl,
