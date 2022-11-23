11daed7231b3a8040ef4f8dfd4a345e0aa977c3c
Martin Creedy <martin@openskydevelopment.co.uk>
2022-11-23 17:10:59 +0000

    remove strictmode

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     // Unsure if this is working or needed
     domains: ["localhost", "https://*.ipfs.w3s.link/"],
