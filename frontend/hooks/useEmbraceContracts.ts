400ec3780917e53bf76804fcc7d9eb7c91ae4d7a
Martin Creedy <martin@openskydevelopment.co.uk>
2022-11-23 13:46:00 +0000

    Remove comments

--- frontend/hooks/useEmbraceContracts.ts
+++ frontend/hooks/useEmbraceContracts.ts
@@ -3,18 +3,6 @@ import EmbraceSpacesJSON from "../data/contractArtifacts/EmbraceSpaces.json";
 import EmbraceAppsJSON from "../data/contractArtifacts/EmbraceApps.json";
 import EmbraceAccountsJSON from "../data/contractArtifacts/EmbraceAccounts.json";
 
-/*
-  // After calling in the contract you can use the contract like this  
-  const { data } = useQuery('prices', async () => {
-    // can call any method on the contract
-    // Get accounts / members etc
-
-    // Can also write to the contract?
-    
-    return result;
-  })
-*/
-
 function useEmbraceContracts() {
   const { data: signer } = useSigner();
   const provider = useProvider();
