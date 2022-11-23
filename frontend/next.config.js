5d99087b7bfb56cdecc113f063e058ac3a520f46
Martin Creedy <martin@openskydevelopment.co.uk>
2022-11-23 15:18:01 +0000

    Update W3 storage URL

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "localhost",
+      "https://bafkreih5noddbpvsuprh4gmhpiyx47gd6z2pripwqelwspqtxumtauyjra.ipfs.w3s.link/",
+    ],
+  },
 };
 
 module.exports = nextConfig;
