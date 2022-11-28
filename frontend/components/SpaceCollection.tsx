b633701dd12ef8054f447cd64d3b589592b8bee5
Martin Creedy <martin@openskydevelopment.co.uk>
2022-11-28 10:56:51 +0000

    use effect dependancy

--- frontend/components/SpaceCollection.tsx
+++ frontend/components/SpaceCollection.tsx
@@ -61,11 +61,11 @@ export default function SpaceCollection({
 
   // Set all images as loaded after 15 seconds i.e. timed out which will hide all loading placeholders
   useEffect(() => {
-    const timeout = setTimeout(() => {
+    setTimeout(() => {
       setMetadataImagesLoaded(collection.map((_, index) => index));
       console.log("Some images amy have timed out");
     }, 15000);
-  }, []);
+  }, [collection]);
 
   function setImageLoaded(index: number) {
     const imagesLoaded = [...metadataImagesLoaded, index];
