121322640eb365d6d94cf21cdb2fe702d5884347
Martin Creedy <martin@openskydevelopment.co.uk>
2022-12-01 15:09:36 +0000

    tailwind screens

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,14 +6,27 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      sans: ['Poppins', 'sans-serif'],
-      serif: ['Poppins', 'serif'],
+      sans: ["Poppins", "sans-serif"],
+      serif: ["Poppins", "serif"],
     },
     extend: {
       colors: {
-      'embracebg': '#F7F6E8',
-      'embracedark': '#35281E'
-      }
+        embracebg: "#F7F6E8",
+        embracedark: "#35281E",
+      },
+    },
+    screens: {
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
     },
   },
   plugins: [require("@tailwindcss/forms")],
